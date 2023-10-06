e14818e56489e696f2e74523db38a26f1a37d965
hossein derees <hosseinderees7@gmail.com>
2023-10-06 02:35:05 -0700

    I add some style for aplication

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
 function main() {
-  const btnExperiment = document.querySelector(".opentExperimentBtn"),
+  const alertMassage = document.querySelectorAll(".alert"),
+    btnExperiment = document.querySelector(".opentExperimentBtn"),
     addExperimentContainer = document.querySelector(".addExperimentContainer"),
     enteredExperimentName = document.querySelector(".enteredExperimentName"),
     addExperimentBtn = document.querySelector(".addExperimentBtn"),
     experimentName = document.querySelector(".experimentName"),
     searchBtn = document.querySelector(".searchBtn"),
-    experiments = document.querySelector(".experiments");
+    experiments = document.querySelector(".experiments"),
+    emptyContainer = document.querySelector(".emptyContainer");
 
   localStorage.getItem("data") === null
     ? localStorage.setItem("data", "[]")
     : localStorage.getItem("data");
+
   let getData = JSON.parse(localStorage.getItem("data")),
     isName = false;
   function addClass(element, clas) {
@@ -18,8 +21,9 @@ function main() {
   function removeClass(element, clas) {
     element.classList.remove(clas);
   }
-
+  // function for create the items or A item
   function createItem(name, status, index) {
+    // create Elements
     let div = document.createElement("div");
     let statusContainer = document.createElement("div");
     let span = document.createElement("span");
@@ -27,43 +31,79 @@ function main() {
     let p = document.createElement("p");
     let i = document.createElement("i");
 
-    experiments.appendChild(div);
-    div.classList.add("item");
-    p.textContent = name;
-    span2.classList.add("status");
-    span2.textContent = status;
-    span2.title = "برای تغییر وضعیت کلیک کنید";
-    statusContainer.addEventListener("click", () => {
-      statusContainer.parentElement.classList.toggle("complate");
-      statusContainer.parentElement.classList.contains("complate")
-        ? (span2.textContent = "تکمیل شده")
-        : (span2.textContent = "تکمیل نشده");
-      if (div.classList.contains("complate")) getData[index].isComplate = true;
-      else getData[index].isComplate = false;
-      console.log(getData);
-      localStorage.setItem("data", JSON.stringify(getData));
-    });
-    span.textContent = " وضعیت : ";
+    // add classes
     i.classList.add("fa");
     i.classList.add("fa-remove");
+    div.classList.add("item");
+    span2.classList.add("status");
+    statusContainer.classList.add("statusContainer");
+    // element content
+    p.textContent = name;
+    span2.textContent = status;
+    span2.title = "برای تغییر وضعیت کلیک کنید";
+    span.textContent = " وضعیت : ";
+    statusContainer.addEventListener("click", () => {
+      statusContainer.parentElement.classList.toggle("complete");
+      statusContainer.parentElement.classList.contains("complete")
+        ? (span2.textContent = "تکمیل شده")
+        : (span2.textContent = "تکمیل نشده");
+      if (div.classList.contains("complete")) getData[index].isComplate = true;
+      else getData[index].isComplate = false;
+      localStorage.setItem("data", JSON.stringify(getData));
+    });
+
     i.onclick = () => {
       let filtered = getData.filter((item) => item.name != p.textContent);
       getData = filtered;
       localStorage.setItem("data", JSON.stringify(getData));
       div.remove();
+      isEmpty();
     };
+    // append the element in experiment
     div.appendChild(p);
     span.appendChild(span2);
     statusContainer.appendChild(span);
     div.appendChild(statusContainer);
     div.appendChild(i);
+    experiments.appendChild(div);
   }
+
+  // Is complete function for check the items complated or no
   function isComplateFn() {
     let items = document.querySelectorAll(".item");
     getData.forEach((item, index) => {
       if (item.isComplate !== null) {
-        if (item.isComplate === true) items[index].classList.add("complate");
-        else items[index].classList.remove("complate");
+        if (item.isComplate === true) items[index].classList.add("complete");
+        else items[index].classList.remove("complete");
+      }
+    });
+  }
+  function isEmpty() {
+    if (experiments.children[1] === undefined) {
+      emptyContainer.classList.add("empty");
+    } else emptyContainer.classList.remove("empty");
+  }
+  function searchFunc() {
+    let items = [...document.querySelectorAll(".item")];
+    items.forEach((item) => {
+      if (!experimentName.value) {
+        return item.classList.remove("hide");
+      }
+      if (item.checkVisibility()) isTrue = true;
+      else isTrue = false;
+      if (!isTrue) {
+        alertMassage[1].classList.add("active");
+        alertMassage[1].children[0].textContent = "اسم وجود ندارد";
+        if (clearInterval(timeout) !== undefined) return clearInterval(timeout);
+        timeout = setTimeout(() => {
+          alertMassage[1].classList.remove("active");
+        }, 2000);
+      }
+      item.classList.add("hide");
+      if (item.querySelector("p").textContent.includes(experimentName.value)) {
+        item.classList.remove("hide");
+      } else {
+        item.classList.add("hide");
       }
     });
   }
@@ -75,6 +115,7 @@ function main() {
     );
   });
   isComplateFn();
+  isEmpty();
   btnExperiment.onclick = () => {
     addClass(addExperimentContainer, "active");
   };
@@ -82,13 +123,31 @@ function main() {
     if (e.target !== addExperimentContainer) return;
     removeClass(addExperimentContainer, "active");
   };
+  let timeout;
   addExperimentBtn.onclick = () => {
-    let index;
     getData.forEach((item) => {
       if (item.name === enteredExperimentName.value.trim()) isName = true;
       else isName = false;
     });
-    if (isName || !enteredExperimentName.value.trim()) return;
+    if (!enteredExperimentName.value.trim()) {
+      alertMassage[0].classList.add("active");
+      alertMassage[0].children[0].textContent = "اسم را خالی نزارید";
+      if (clearInterval(timeout) !== undefined) return clearInterval(timeout);
+      timeout = setTimeout(() => {
+        alertMassage[0].classList.remove("active");
+      }, 2000);
+      isName = false;
+      return;
+    } else if (isName) {
+      alertMassage[0].classList.add("error");
+      alertMassage[0].children[0].textContent = "اسم در لیست وجود دارد";
+      if (clearInterval(timeout) !== undefined) return clearInterval(timeout);
+      timeout = setTimeout(() => {
+        alertMassage[0].classList.remove("error");
+      }, 2000);
+      isName = false;
+      return;
+    }
     let item = {
       name: enteredExperimentName.value.trim(),
       isComplate: false,
@@ -97,26 +156,22 @@ function main() {
     localStorage.setItem("data", JSON.stringify(getData));
     createItem(enteredExperimentName.value.trim(), "تکمیل نشده");
     removeClass(addExperimentContainer, "active");
+    isEmpty();
   };
+  let isTrue = false;
   experimentName.addEventListener("keyup", () => {
-    let items = [...document.querySelectorAll(".item")];
-    if (!experimentName.value) {
-      items.forEach((item) => {
-        item.classList.remove("hide");
-      });
-    }
-    console.log(items);
-    items.forEach((item) => {
-      item.classList.add("hide");
-    });
-    items.forEach((item) => {
-      if (item.querySelector("p").textContent.includes(experimentName.value)) {
-        item.classList.remove("hide");
-      } else {
-        item.classList.add("hide");
-      }
-    });
+    searchFunc();
   });
+  searchBtn.onclick = () => {
+    searchBtn.parentElement.classList.toggle("active");
+    if (searchBtn.parentElement.classList.contains("active")) {
+      searchBtn.classList.add("fa-remove");
+      searchBtn.classList.remove("fa-search");
+    } else {
+      searchBtn.classList.add("fa-search");
+      searchBtn.classList.remove("fa-remove");
+    }
+  };
   window.addEventListener("keydown", () => {
     enteredExperimentName.focus();
   });
